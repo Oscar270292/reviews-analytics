338e701b4e1598555601fc6868371ac8ba5b143b
Oscar Ou <warriorsoscar@gmail.com>
2021-05-24 16:42:31 +0800

    screen fast

--- rev-analytics.py
+++ rev-analytics.py
@@ -11,3 +11,18 @@ b = 0
 for d in data:
 	b += len(d)
 print('留言的平均長度為', b / 1000000)
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('共有', len(new), '筆資料長度小於100')
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good),'筆資料提到good' )
+print(good[2])
+
+disappointed = [d for d in data if 'disappointed' in d]
+print(len(d))
+print(disappointed[766])
