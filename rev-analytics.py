c817d43f1ba1dd72116e8f9d05f17a4940f0b731
Oscar Ou <warriorsoscar@gmail.com>
2021-05-24 16:10:14 +0800

    cala average reviews

--- rev-analytics.py
+++ rev-analytics.py
@@ -6,3 +6,8 @@ with open('reviews.txt', 'r') as f:
 		i += 1
 		if i % 1000 == 0:
 			print(i)
+print('讀取完了，總共有', len(data), '筆資料')
+b = 0
+for d in data:
+	b += len(d)
+print('留言的平均長度為', b / 1000000)
